1c4f7e3cabc7a2f5ccb429acee9bd575b6879468
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-07-20 11:27:10 +0530

    Time: 6 ms (55.4%), Space: 47.3 MB (44.14%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,30 +1,34 @@
 class Solution {
+    private void helper(int[][]grid , int left , int right){
+        int n = grid.length;
+        int m = grid[0].length;
+        while(left < right){
+            int leftRow = left/m;
+            int leftCol = left%m;
+            int rightRow = right/m;
+            int rightCol = right%m;
+            int temp = grid[leftRow][leftCol];
+            grid[leftRow][leftCol] = grid[rightRow][rightCol];
+            grid[rightRow][rightCol] = temp;
+            left++;
+            right--;
+        }
+        return;
+    }
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int n = grid.length;
         int m = grid[0].length;
         k = k%(n*m);
         List<List<Integer>> answer = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        for(int i = 0;i<k;i++){
-            int idx = n*m-k+i;
-            int currCol = idx%m;
-            int currRow = idx/m;
-            temp.add(grid[currRow][currCol]);
-            if(temp.size() == m){
-                answer.add(temp);
-                temp = new ArrayList<>();
+        helper(grid , 0 , n*m-k-1);
+        helper(grid , n*m-k , n*m-1);
+        helper(grid , 0 , n*m-1);
+        for(int i = 0;i<n;i++){
+            List<Integer> temp = new ArrayList<>();
+            for(int j = 0;j<m;j++){
+                temp.add(grid[i][j]);
             }
-        }
-        for(int i = 0;i<n*m-k;i++){
-            int currCol = i%m;
-            int currRow = i/m;
-            int num = grid[currRow][currCol];
-            temp.add(num);
-            if(temp.size() == m){
-                answer.add(temp);
-                temp = new ArrayList<>();
-            }
-
+            answer.add(temp);
         }
         return answer;
         
