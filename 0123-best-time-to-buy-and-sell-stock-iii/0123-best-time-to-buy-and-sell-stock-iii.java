1728b08ef255c445b25c3a2d6b061ce6de3037ce
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-07-25 17:25:28 +0530

    Time: 128 ms (15.79%), Space: 132.8 MB (23.55%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,35 @@
+class Solution {
+    private int helper(int[]prices , int index , int buy , int k , int[][][]dp){
+        int n = prices.length;
+        if(index == n){
+            return 0;
+        }
+        if(k == 2){
+            return 0;
+        }
+        if(dp[index][buy][k] != -1)return dp[index][buy][k];
+        int one = 0;
+        int two = 0;
+        if(buy == 0){
+            one = helper(prices , index+1 , 1 , k , dp)-prices[index];
+            two = helper(prices , index+1 , 0 , k , dp);
+        }
+        if(buy == 1){
+            one = helper(prices , index+1 , 0 , k+1 , dp)+prices[index];
+            two = helper(prices , index+1 , buy , k , dp);
+        }
+        return dp[index][buy][k] = Math.max(one , two);
+    }
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][][]dp = new int[n][2][3];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<2;j++){
+                Arrays.fill(dp[i][j] , -1);
+            }
+        }
+        return helper(prices , 0 , 0 , 0 , dp);
+
+        
+    }
+}
