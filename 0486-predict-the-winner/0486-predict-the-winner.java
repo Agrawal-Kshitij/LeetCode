de35c2727e093905a829f4f39781667d1831d1d4
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-08-02 13:21:41 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.72%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,13 +1,18 @@
 class Solution {
-    private int helper(int[]nums , int i , int j){
+    private int helper(int[]nums , int i , int j , int[][]dp){
         if(i > j)return 0;
-        int one = nums[i]+Math.min(helper(nums , i+2 , j) , helper(nums , i+1 , j-1));
-        int two = nums[j]+Math.min(helper(nums , i+1 , j-1) , helper(nums , i , j-2));
-        return Math.max(one , two);
+        if(dp[i][j] != -1)return dp[i][j];
+        int one = nums[i]+Math.min(helper(nums , i+2 , j , dp) , helper(nums , i+1 , j-1 , dp));
+        int two = nums[j]+Math.min(helper(nums , i+1 , j-1 , dp) , helper(nums , i , j-2 , dp));
+        return dp[i][j] = Math.max(one , two);
     }
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        int A = helper(nums , 0 , n-1);
+        int[][]dp = new int[n+1][n+1];
+        for(int i = 0;i<=n;i++){
+            Arrays.fill(dp[i] , -1);
+        }
+        int A = helper(nums , 0 , n-1 , dp);
         int sum = 0;
         for(int i = 0;i<n;i++){
             sum+=nums[i];
