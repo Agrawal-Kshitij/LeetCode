f5094aeb2d058240768dd5ad187c1f35f0595ab3
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-08-02 13:19:39 +0530

    Time: 97 ms (11.09%), Space: 42.7 MB (57.72%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,22 +1,19 @@
 class Solution {
-    private boolean helper(int[]nums , int index1 , int index2 , boolean chance , int scoreA , int scoreB){
-        if(index1 > index2){
-            return scoreA>=scoreB;
-        }
-        if(chance == true){
-            boolean ans1 = helper(nums , index1+1 , index2 , false , scoreA+nums[index1] , scoreB);
-            boolean ans2 = helper(nums , index1 , index2-1 , false , scoreA+nums[index2] , scoreB);
-            return ans1||ans2;
-        }
-        else{
-            boolean ans1 = helper(nums , index1+1 , index2 , true , scoreA , scoreB+nums[index1]);
-            boolean ans2 = helper(nums , index1 , index2-1 , true , scoreA , scoreB+nums[index2]);
-            return ans1 && ans2;
-        }
+    private int helper(int[]nums , int i , int j){
+        if(i > j)return 0;
+        int one = nums[i]+Math.min(helper(nums , i+2 , j) , helper(nums , i+1 , j-1));
+        int two = nums[j]+Math.min(helper(nums , i+1 , j-1) , helper(nums , i , j-2));
+        return Math.max(one , two);
     }
     public boolean predictTheWinner(int[] nums) {
         int n = nums.length;
-        return helper(nums , 0 , n-1 , true , 0 , 0);
+        int A = helper(nums , 0 , n-1);
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+            sum+=nums[i];
+        }
+        int B = sum-A;
+        return A>=B;
         
     }
 }
