aac927175382f1a9959425415f563e0c8c37797f
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-08-03 11:10:33 +0530

    Time: 37 ms (40.34%), Space: 92.1 MB (43.18%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -3,15 +3,11 @@ class Solution {
         int n = stoneValue.length;
         if(i >= stoneValue.length)return 0;
         if(dp[i] != -1)return dp[i];
-        int one = stoneValue[i] + Math.min(helper(i+2 , stoneValue , dp) , Math.min(helper(i+3 , stoneValue , dp) , helper(i+4 , stoneValue , dp)));
+        int one = stoneValue[i]-helper(i+1 , stoneValue , dp);
         int two = Integer.MIN_VALUE;
+        if(i+1 < n)two = stoneValue[i]+stoneValue[i+1]-helper(i+2 , stoneValue , dp);
         int three = Integer.MIN_VALUE;
-        if(i+1 < n){
-            two = stoneValue[i]+stoneValue[i+1]+Math.min(helper(i+5 , stoneValue , dp) , Math.min(helper(i+3 , stoneValue , dp) , helper(i+4 , stoneValue , dp)));
-        }
-        if(i+2 < n){
-            three = stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]+Math.min(helper(i+4 , stoneValue , dp) , Math.min(helper(i+5 , stoneValue , dp) , helper(i+6 , stoneValue , dp)));
-        }
+        if(i+2 < n)three = stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-helper(i+3 , stoneValue , dp);
         return dp[i] = Math.max(one , Math.max(two , three));
     }
     public String stoneGameIII(int[] stoneValue) {
@@ -19,13 +15,8 @@ class Solution {
         int[]dp = new int[n+1];
         Arrays.fill(dp , -1);
         int A = helper(0 , stoneValue , dp);
-        int sum = 0;
-        for(int i = 0;i<n;i++){
-            sum+=stoneValue[i];
-        }
-        int B = sum-A;
-        if(A > B)return "Alice";
-        if(A < B)return "Bob";
+        if(A > 0)return "Alice";
+        if(A < 0)return "Bob";
         return "Tie";
         
     }
