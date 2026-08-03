cfe20e942358eee09923099ef9817abab3986606
Kshitij Agrawal <kshitijagrawal567@gmail.com>
2026-08-03 11:13:48 +0530

    Time: 4 ms (98.01%), Space: 85.6 MB (60.23%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -13,8 +13,17 @@ class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
         int[]dp = new int[n+1];
-        Arrays.fill(dp , -1);
-        int A = helper(0 , stoneValue , dp);
+        // Arrays.fill(dp , -1);
+        // int A = helper(0 , stoneValue , dp);
+        for(int i = n-1;i>=0;i--){
+            int one = stoneValue[i]-dp[i+1];
+            int two = Integer.MIN_VALUE;
+            int three = Integer.MIN_VALUE;
+            if(i+1 < n)two = stoneValue[i]+stoneValue[i+1]-dp[i+2];
+            if(i+2 < n)three = stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3];
+            dp[i] = Math.max(one , Math.max(two , three));
+        }
+        int A = dp[0];
         if(A > 0)return "Alice";
         if(A < 0)return "Bob";
         return "Tie";
